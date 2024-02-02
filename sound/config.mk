08fe9e4a2373b0e8e6d65cd8a735af39efcca747
horhik <horhik@disroot.org>
2024-02-03 01:56:56 +0300

    updated gitigore

--- sound/config.mk
+++
@@ -1,2 +0,0 @@
-SDL_CFLAGS=-D_REENTRANT -I/nix/store/sfdik4xry5h1d34h60c119464dwswz51-SDL2-2.26.5-dev/include -I/nix/store/sfdik4xry5h1d34h60c119464dwswz51-SDL2-2.26.5-dev/include/SDL2
-SDL_LDLIBS=-L/nix/store/r8v664ihs6jhcaskbd8w4kzb12fsy6yv-SDL2-2.26.5/lib -Wl,-rpath,/nix/store/r8v664ihs6jhcaskbd8w4kzb12fsy6yv-SDL2-2.26.5/lib -Wl,--enable-new-dtags -lSDL2
